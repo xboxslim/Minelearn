7a377474755d333b29eda0a114d7b11aa1603f06
xboxslim <xboxslim788@gmail.com>
2021-03-18 20:37:24 -0300

    Atualizando Arvore.c com struct e transformando em library

--- src/Arvore.c
+++ src/Arvore.c
@@ -2,63 +2,47 @@
 //criando Arvores
 
 #include <stdio.h>
-
-
 //-----------------------------/
 //Variaveis
 
-int arvore;
-int arvoreSemente;
-int arvoreFolha;
-int arvoreMadeira;
-int arvoreNome;
-int arvoreId;
+char db_Arvores_Mundo[][20]=
+{"Carvalho",
+ "Macieira",
+ "Pinheiro",
+ "Cedro"
+};
 
-//-----------------------------/
+int tam_db_Arvores_Mundo = ((sizeof db_Arvores_Mundo)/20);
 
-int main(){
+struct Arvores_Mundo
+{
+	int arvore;
+	int arvoreSemente;
+	int arvoreFolha;
+	int arvoreMadeira;
+	int arvoreNome;
+	int arvoreId;	
+}arvor;
 
-//ação
-
-arvoreId=1;
-arvoreSemente=1;
-arvoreFolha=1;
-arvoreMadeira=3;
-
-// tem que ser criado um banco de nomes
-
-arvoreNome=1;
-
-/*
-
- Carvalho
- Macieira
- Pinheiro
-
-
-*/
-
-	printf("Arvore ID = %d\n",arvoreId);
-	printf("Arvore Semente = %d\n",arvoreSemente);
-	printf("Arvore Folha = %d\n",arvoreFolha);
-	printf("Arvore Madeira = %d\n",arvoreMadeira);
-	
-	if(arvoreNome == 1){
-		printf("Arvore Nome = Carvalho\n");
-	}
-	if(arvoreNome == 2){
-		printf("Arvore Nome = Macieira\n");
-	}
-	if(arvoreNome == 3){
-		printf("Arvore Nome = Pinheiro\n");
-	}
-		
-
-	return 0;
+struct Arvores_Mundo Arvore_Inicializacao(int fID,int Nome)
+{
+	struct Arvores_Mundo farvor;
+	farvor.arvoreId = fID;
+	farvor.arvoreSemente = 2;
+	farvor.arvoreFolha = 15;
+	farvor.arvoreMadeira = 8;
+	farvor.arvoreNome = Nome;
+	return farvor;
 }
 
-
-
-
+void Arvore_Status(struct Arvores_Mundo farvor)
+{
+	printf("Arvore ID = %d\n",farvor.arvoreId);
+	printf("Arvore Semente = %d\n",farvor.arvoreSemente);
+	printf("Arvore Folha = %d\n",farvor.arvoreFolha);
+	printf("Arvore Madeira = %d\n",farvor.arvoreMadeira);
+	printf("Arvore Nome = %s\n",db_Arvores_Mundo[farvor.arvoreNome-1]);
+	printf("tamanho do banco de dados Arvores = %d\n",tam_db_Arvores_Mundo);	
+}
 
 
